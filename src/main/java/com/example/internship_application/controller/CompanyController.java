0c04866057dbd0a17d981fa16809a8ec8fe34424
vincentbharata <vincentbharataa@gmail.com>
2025-06-16 14:53:21 +0700

    hot fix

--- src/main/java/com/example/internship_application/controller/CompanyController.java
+++ src/main/java/com/example/internship_application/controller/CompanyController.java
@@ -1,17 +1,18 @@
 package com.example.internship_application.controller;
 
-import com.example.internship_application.dto.CompanyRequest;
-import com.example.internship_application.model.Company;
-import com.example.internship_application.service.CompanyService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.internship_application.dto.CompanyRequest;
+import com.example.internship_application.model.Company;
+import com.example.internship_application.service.CompanyService;
+
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private CompanyService companyService;
+    private final CompanyService companyService;
 
     public CompanyController(CompanyService companyService){this.companyService = companyService;}
 
--- src/main/java/com/example/internship_application/dto/StudentRequest.java
+++ src/main/java/com/example/internship_application/dto/StudentRequest.java
@@ -1,5 +1,9 @@
 package com.example.internship_application.dto;
 
-public class StudentRequest {
-    
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record StudentRequest(
+    @NotBlank String name,
+    @Email @NotBlank String email
+) {}
