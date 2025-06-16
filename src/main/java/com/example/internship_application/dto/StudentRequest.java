20d1f9cb2ffbf2083704a6abb30ccd0007bc5566
Raven DM <ravendaniel0@gmail.com>
2025-06-16 14:42:23 +0700

    student dto update

--- src/main/java/com/example/internship_application/dto/StudentRequest.java
+++ src/main/java/com/example/internship_application/dto/StudentRequest.java
@@ -1,5 +1,9 @@
 package com.example.internship_application.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class StudentRequest {
-    
+    @NotBlank String name;
+    @Email @NotBlank String email;
 }
